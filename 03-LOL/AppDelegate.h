1970c43667b3dee465e7380548fb295cacd3162a
程玉娟 <1009449682@qq.com>
2015-12-04 14:33:40 +0800

    Initial Commit

---
+++ 03-LOL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  03-LOL
+//
+//  Created by qingyun on 15/12/4.
+//  Copyright (c) 2015年 阿六. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
